73494d48a6ce0f1c107335a72a0a17cff2f869c2
kevin.xb <xxbb1234021@qq.com>
2014-10-31 16:38:16 +0800

    新增随机搜索，爬山，模拟退火算法

--- com/ml/Optimization.py
+++ com/ml/Optimization.py
@@ -35,7 +35,7 @@ def getMinutes(t):
     x = time.strptime(t, '%H:%M')
     return x[3] * 60 + x[4]
   
-#打印航班表格  
+# 打印航班表格  
 def printSchedule(r):
     for d in range(len(r) / 2):
         name = people[d][0]
@@ -44,7 +44,7 @@ def printSchedule(r):
         ret = flights[(destination, origin)][r[2 * d + 1]]
         print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name, origin, out[0], out[1], out[2], ret[0], ret[1], ret[2])
 
-#成本函数
+# 成本函数
 def scheduleCost(sol):
     totalprice = 0
     latestarrival = 0
@@ -81,12 +81,109 @@ def scheduleCost(sol):
       
     return totalprice + totalwait
 
+# 随机搜索
+def randomoptimize(domain, costf):
+    best = 999999999
+    bestr = None
+    for i in range(0, 1000):
+        # 创建随机解
+        r = [random.randint(domain[j][0], domain[j][1]) for j in range(len(domain))]
+    
+        # 得到成本
+        cost = costf(r)
+        
+        # 比较
+        if cost < best:
+            best = cost
+            bestr = r 
+    return r
+
+# 模拟爬山法
+def hillclimb(domain, costf):
+    # 创建随机解
+    sol = [random.randint(domain[i][0], domain[i][1])
+        for i in range(len(domain))]
+    # 循环
+    while 1:
+        # 创建相邻解的列表
+        neighbors = []
+    
+        for i in range(len(domain)):
+            # 在每个方向上相对于原值偏离一点
+            if sol[i] > domain[i][0]:
+                neighbors.append(sol[0:i] + [sol[i] + 1] + sol[i + 1:])
+            if sol[i] < domain[i][1]:
+                neighbors.append(sol[0:i] + [sol[i] - 1] + sol[i + 1:])
+
+        # 在相邻的解中寻找最优解
+        current = costf(sol)
+        best = current
+        for i in range(len(neighbors)):
+            #print i
+            cost = costf(neighbors[i])
+            if cost < best:
+                best = cost
+                sol = neighbors[i]
+
+        # 如果没有更好的解，则退出
+        if best == current:
+            break
+    return sol
+
+# 模拟退火算法
+def annealingoptimize(domain, costf, T=10000.0, cool=0.95, step=1):
+    # 创建随机解
+    vec = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+  
+    while T > 0.1:
+        # 选择一个索引值
+        i = random.randint(0, len(domain) - 1)
+    
+        # 选择一个改变索引值得方向
+        dir = random.randint(-step, step)
+    
+        # 创建一个代表题解的新列表，改变其中一个值
+        vecb = vec[:]
+        vecb[i] += dir
+        if vecb[i] < domain[i][0]: 
+            vecb[i] = domain[i][0]
+        elif vecb[i] > domain[i][1]: 
+            vecb[i] = domain[i][1]
+    
+        # 计算当前成本
+        ea = costf(vec)
+        eb = costf(vecb)
+        p = pow(math.e, (-eb - ea) / T)
+    
+        # Is it better, or does it make the probability
+        # cutoff?
+        if (eb < ea or random.random() < p):
+            vec = vecb      
+    
+        # 降低温度
+        T = T * cool
+    return vec
+
 
 def main():
     # print filePath
-    s = [1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
-    printSchedule(s)
+    # s = [1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
+    # printSchedule(s)
+    # print scheduleCost(s)
+    domain = [(0, 9)] * len(people) * 2
+    '''
+    print domain
+    s = randomoptimize(domain, scheduleCost)
+    print s
     print scheduleCost(s)
-
+    printSchedule(s)
+    '''
+    #s = hillclimb(domain, scheduleCost)
+    
+    s = annealingoptimize(domain, scheduleCost)
+    print scheduleCost(s)
+    printSchedule(s)
+    
+    
 if __name__ == '__main__':
     main()
